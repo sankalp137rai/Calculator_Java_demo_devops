e51b4bffb786e01580b791bc9e29f9495f227aa9
Sankalp Rai <sankalp137rai@gmail.com>
2024-02-14 13:22:09 +0530

    adding files

---
+++ src/main/java/com/example/demo_calcalutor_spe/DemoCalculatorSpeApplication.java
@@ -0,0 +1,137 @@
+package com.example.demo_calcalutor_spe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+@SpringBootApplication
+//public class DemoCalculatorSpeApplication {
+public class DemoCalculatorSpeApplication {
+
+	private static final Logger logger = LogManager.getLogger(DemoCalculatorSpeApplication.class);
+	public DemoCalculatorSpeApplication() {
+	}
+
+	public static void main(String[] args) {
+
+		DemoCalculatorSpeApplication cal = new DemoCalculatorSpeApplication();
+		Scanner scanner = new Scanner(System.in);
+		double num1, num2;
+		do {
+			System.out.println("Calculator using DevOps. \n Choose:");
+			System.out.print("1. Factorial\n2. Square root\n3. Power\n4. Natural Logarithm\n" +
+					"5. Exit\nEnter your choice: ");
+			int x;
+			try {
+				x = scanner.nextInt();
+			} catch (InputMismatchException error) {
+				return;
+			}
+
+			switch (x) {
+				case 1:
+					// Factorial
+					System.out.print("Enter a number : ");
+					num1 = scanner.nextDouble();
+					System.out.println("Factorial of "+ num1 +" is : " + cal.factorial(num1));
+					System.out.println("\n");
+					break;
+
+				case 2:
+					// Square root
+					System.out.print("Enter a number : ");
+					num1 = scanner.nextDouble();
+					System.out.println("Square root of "+num1+" is : " + cal.squareRoot(num1));
+					System.out.println("\n");
+					break;
+
+				case 3:
+					// Power
+					System.out.print("Enter the first number : ");
+					num1 = scanner.nextDouble();
+					System.out.print("Enter the second number : ");
+					num2 = scanner.nextDouble();
+					System.out.println(num1+ " raised to power "+num2+" is : " + cal.power(num1, num2));
+					System.out.println("\n");
+					break;
+
+				case 4:
+					// Natural log
+					System.out.print("Enter a number : ");
+					num1 = scanner.nextDouble();
+					System.out.println("Natural log of "+num1+" is : " + cal.naturalLog(num1));
+					System.out.println("\n");
+					break;
+
+				default:
+					System.out.println("Exit");
+					return;
+			}
+		} while (true);
+	}
+
+
+	public double factorial(double number1) {
+		logger.info("[FACTORIAL] - " + number1);
+		double result = fact(number1);
+		logger.info("[RESULT - FACTORIAL] - " + result);
+		return result;
+	}
+
+	public double fact(double num) {
+		double facto = 1;
+		for(int i = 1; i <= num; ++i)
+		{ facto *= i;   }
+		return  facto;
+	}
+
+	public double squareRoot(double number1) {
+		logger.info("[SQ ROOT] - " + number1);
+		double result = 0;
+		try{
+			if(number1 < 0){
+				throw new Exception("Case of Imaginary Number");
+			}
+			result = Math.sqrt(number1);
+		}
+		catch (Exception error){
+			logger.error("[EXCEPTION - LOG] - Cannot find square root of negative numbers");
+			return Double.NaN;
+		}
+		logger.info("[RESULT - SQ ROOT] - " + result);
+		return result;
+	}
+
+
+	public double power(double number1, double number2) {
+		logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
+		double result = Math.pow(number1,number2);
+		logger.info("[RESULT - POWER] - " + result);
+		return result;
+	}
+
+	public double naturalLog(double number1) {
+		logger.info("[NATURAL LOG] - " + number1);
+		double result = 0;
+		try {
+
+			if (number1 <0 ) {
+				result = Double.NaN;
+				throw new ArithmeticException("Case of NaN 0.0/0.0");
+			}
+			else {
+				result = Math.log(number1);
+			}
+		} catch (ArithmeticException error) {
+			logger.error("[EXCEPTION - LOG] - Cannot find logarithm of negative numbers");
+			return Double.NaN;
+		}
+		logger.info("[RESULT - NATURAL LOG] - " + result);
+		return result;
+	}
+
+}
---
+++ src/test/java/com/example/demo_calcalutor_spe/DemoCalculatorSpeApplicationTests.java
@@ -0,0 +1,59 @@
+package com.example.demo_calcalutor_spe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+//@SpringBootTest
+public class DemoCalculatorSpeApplicationTests {
+
+	private static final double DELTA = 1e-15;
+	DemoCalculatorSpeApplication calculator = new DemoCalculatorSpeApplication();
+
+	@Test
+	public void factorialTruePositive() {
+		assertEquals(720, calculator.fact(6), DELTA);
+		assertEquals(1, calculator.fact(0), DELTA);
+	}
+
+	@Test
+	public void factorialFalsePositive() {
+		assertNotEquals(113, calculator.fact(5), DELTA);
+		assertNotEquals(10, calculator.fact(6), DELTA);
+	}
+
+	@Test
+	public void powerTruePositive() {
+		assertEquals(81, calculator.power(3, 4), DELTA);
+		assertEquals(25, calculator.power(5, 2), DELTA);
+	}
+
+	@Test
+	public void powerFalsePositive() {
+		assertNotEquals(6, calculator.power(2, 3), DELTA);
+		assertNotEquals(-7, calculator.power(3, 3), DELTA);
+	}
+
+	@Test
+	public void logTruePositive() {
+		assertEquals(0, calculator.naturalLog(1), DELTA);
+		assertEquals(1, calculator.naturalLog(2.718281828459045), DELTA);
+	}
+
+	@Test
+	public void logFalsePositive() {
+		assertNotEquals(60, calculator.naturalLog(2.4), DELTA);
+		assertNotEquals(4.7, calculator.naturalLog(2.1), DELTA);
+	}
+
+	@Test
+	public void squareRootTruePositive() {
+		assertEquals(4, calculator.squareRoot(16), DELTA);
+		assertEquals(6, calculator.squareRoot(36), DELTA);
+	}
+
+	@Test
+	public void squareRootFalsePositive() {
+		assertNotEquals(1, calculator.squareRoot(3), DELTA);
+		assertNotEquals(4, calculator.squareRoot(81), DELTA);
+	}
+}
